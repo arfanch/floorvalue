6cb49c454161cbbcd33540b37f762395486b7fbb
sreedharachandra <sreedharachandra@gmail.com>
2021-10-29 08:50:29 +0530

    test

--- src/App.js
+++ src/App.js
@@ -591,47 +591,7 @@ export default function App() {
             Twitter and if you enjoy the website please share it.</Text> 
         </Container> 
       </Box>     
-          <Box
-          mb={0}
-      bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}>
-      <Container
-        as={Stack}
-        maxW={'6xl'}
-        py={4}
-        spacing={4}
-        justify={'center'}
-        align={'center'}>
-        <Logo />
-        <Stack direction={'row'} spacing={6}>
-          <Link href={'#'}>Home</Link>
-          <Link href={'#'}>About</Link>
-          <Link href={'#'}>Blog</Link>
-          <Link href={'#'}>Contact</Link>
-        </Stack>
-      </Container>
-
-      <Box
-        borderTopWidth={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.700')}>
-        <Container
-          as={Stack}
-          maxW={'6xl'}
-          py={4}
-          direction={{ base: 'column', md: 'row' }}
-          spacing={4}
-          justify={{ base: 'center', md: 'space-between' }}
-          align={{ base: 'center', md: 'center' }}>
-          <Text>© 2020 ElloInc. All rights reserved</Text>
-          <Stack direction={'row'} spacing={6}>
-            <SocialButton label={'Twitter'} href={'https://twitter.com/livingpixelated'}>
-              <FaTwitter />
-            </SocialButton> 
-          </Stack>
-        </Container>
-      </Box>
-    </Box>
+          
     </Box>
   );
 }
